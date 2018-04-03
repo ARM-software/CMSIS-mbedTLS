82a64c1a53a7f3d47f3308ac740ea6297fb155a9
Robert Rostohar <Robert.Rostohar@arm.com>
2018-04-03 12:26:59 +0200

    mbedTLS pack: Updated MDK examples (minor)

--- MDK/examples/ssl_server/ssl_server.c
+++ MDK/examples/ssl_server/ssl_server.c
@@ -62,7 +62,7 @@ int main( void )
 #include "mbedtls/certs.h"
 #include "mbedtls/x509.h"
 #include "mbedtls/ssl.h"
-#include "mbedtls/net.h"
+#include "mbedtls/net_sockets.h"
 #include "mbedtls/error.h"
 #include "mbedtls/debug.h"
 
