a4197c3769bb2e2e821143d8dda75d8e14c2ef3b
Robert Rostohar <Robert.Rostohar@arm.com>
2021-02-15 11:20:27 +0100

    mbedTLS pack: add MDK examples for MCB4300 board

---
+++ MDK/examples/MCB4300/ssl_server/main.c
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------
+ * CMSIS-RTOS 'main' function template
+ *---------------------------------------------------------------------------*/
+
+#include "RTE_Components.h"
+#include  CMSIS_device_header
+#include "cmsis_os2.h"
+
+/*----------------------------------------------------------------------------
+ * Application main thread
+ *---------------------------------------------------------------------------*/
+#define APP_MAIN_STK_SZ (8192)
+static uint64_t app_main_stk[APP_MAIN_STK_SZ/8];
+
+static const osThreadAttr_t app_main_attr = {
+  .stack_mem  = &app_main_stk[0],
+  .stack_size = sizeof(app_main_stk)
+};
+
+extern int ssl_server(void);
+
+__NO_RETURN static void app_main (void *argument) {
+  (void)argument;
+
+  ssl_server();
+  for (;;) {}
+}
+
+int main (void) {
+
+  // System Initialization
+  SystemCoreClockUpdate();
+
+  osKernelInitialize();                         // Initialize CMSIS-RTOS
+  osThreadNew(app_main, NULL, &app_main_attr);  // Create application main thread
+  osKernelStart();                              // Start thread execution
+  for (;;) {}
+}
